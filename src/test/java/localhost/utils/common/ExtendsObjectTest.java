3c85d466ba325b05d80b1863d6eff53fad52177b
viktorov_maxim <victorov_maxim@mail.ru>
2022-08-18 19:48:58 +0300

    fillingFieldsObjectUtil

---
+++ src/test/java/localhost/utils/common/ExtendsObjectTest.java
@@ -0,0 +1,14 @@
+package localhost.utils.common;
+
+public class ExtendsObjectTest {
+  private boolean booleanField;
+  private Boolean booleanFieldClass;
+
+  public boolean isBooleanField() {
+    return booleanField;
+  }
+
+  public Boolean getBooleanFieldClass() {
+    return booleanFieldClass;
+  }
+}
